57bf0df4ab920bf14920ddf6a1fe9f1fc51e0315
seohee-P <p03061180@gmail.com>
2023-07-06 16:21:44 +0900

    WebSecurityConfig.java modify

--- src/main/java/com/example/knuleeproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/knuleeproject/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
-                .requestMatchers(toH2Console())
+                //.requestMatchers(toH2Console())
                 .antMatchers("/static/**");
     }
 
